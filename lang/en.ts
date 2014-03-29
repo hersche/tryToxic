40e60ee87613c880fd0e039f0288c0393621ac5c
skamster <hersche@vmail.me>
2014-03-29 15:41:48 +0100

    get the command for generating lang/ts-files again qmake -project -o ts.pro

---
+++ lang/en.ts
@@ -0,0 +1,173 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="de_CH" sourcelanguage="">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="main_gui_integradet.py" line="510"/>
+        <source>Config</source>
+        <translation type="obsolete">Konfiguration</translation>
+    </message>
+    <message>
+        <location filename="main_gui_integradet.py" line="650"/>
+        <source>Spesen</source>
+        <translation type="obsolete">Charges</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="ui/main.ui" line="298"/>
+        <source>Create</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="305"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="312"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="285"/>
+        <source>Value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="611"/>
+        <source>Lohn</source>
+        <translation type="obsolete">Loan</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="613"/>
+        <source>Zeit (ges)</source>
+        <translation type="obsolete">Time</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="615"/>
+        <source>Spesen</source>
+        <translation type="obsolete">Charges</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="617"/>
+        <source>Abz&#xc3;&#xbc;ge</source>
+        <translation type="obsolete">Splits</translation>
+    </message>
+    <message>
+        <location filename="main.py" line="624"/>
+        <source>Summe (gesammt)</source>
+        <translation type="obsolete">Sum (@all)</translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="268"/>
+        <source>Configs</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="278"/>
+        <source>Key</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="14"/>
+        <source>tryTox</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="41"/>
+        <source>ToxTry</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="76"/>
+        <source>Send</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="102"/>
+        <source>Your ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="109"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="116"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="130"/>
+        <source>Online</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="135"/>
+        <source>Away</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="140"/>
+        <source>Busy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="148"/>
+        <source>Statusmessage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="155"/>
+        <source>I&apos;m alive!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="171"/>
+        <source>Friendlist</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="184"/>
+        <source>FriendInfos</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="225"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:14px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:large; font-weight:600;&quot;&gt;Notifications&lt;/span&gt;&lt;/p&gt;
+&lt;hr /&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="244"/>
+        <source>Add new Friend</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="250"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="ui/main.ui" line="334"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Ubuntu&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;; font-size:18pt; text-decoration: underline;&quot;&gt;Avaible Config-Keys&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;;&quot;&gt;* lang / language = language - put the right filename (*.qm in your folder) there&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;;&quot;&gt;* encrypted : 1=CAST, 2=Blowfish, 3=DES3,4=ARC4,5=XOR,6=AES,7=ARC2 (you need to install pycrypto / python3-crypto @ *buntu). Put &amp;quot;None&amp;quot; as value for going back to cleartext. Encryption shouldn&apos;t need any restart.&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-family:&apos;Sans Serif&apos;;&quot;&gt;Restart is needed after changes!&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
