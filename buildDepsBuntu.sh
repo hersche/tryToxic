f8a7c26af5def5bb663ba658cc4fb150b6256caa
skamster <hersche@vmail.me>
2014-04-05 15:35:05 +0200

    add av-support for tox

--- buildDepsBuntu.sh
+++ buildDepsBuntu.sh
@@ -2,6 +2,7 @@
 sudo apt-get install build-essential libtool autotools-dev automake checkinstall check git yasm python3-crypto python3-pyqt4 python3-dev
 mkdir build
 cd build
+#libsodium
 git clone git://github.com/jedisct1/libsodium.git
 cd libsodium
 ./autogen.sh
@@ -9,6 +10,24 @@ cd libsodium
 sudo checkinstall --install --pkgname libsodium --pkgversion 0.4.2 --nodoc
 sudo ldconfig
 cd ..
+#opus
+wget http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
+tar xzf opus-1.0.3.tar.gz
+cd opus-1.0.3
+./configure
+make -j3
+sudo make install
+cd ..
+#libvpx
+git clone http://git.chromium.org/webm/libvpx.git
+cd libvpx
+./configure --enable-shared
+make -j3
+sudo make install
+sudo ldconfig
+cd ..
+
+#tox-core
 git clone git://github.com/irungentoo/ProjectTox-Core.git
 cd ProjectTox-Core
 autoreconf -i
@@ -16,6 +35,7 @@ autoreconf -i
 make
 sudo make install
 cd ..
+#pytox
 git clone https://github.com/aitjcize/PyTox.git 
 cd PyTox
 sudo python3 setup.py install
