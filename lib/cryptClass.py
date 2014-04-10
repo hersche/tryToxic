5a3ae1ba669ee5685660e651671962a763e8b280
skamster <hersche@vmail.me>
2014-04-10 15:32:38 +0200

    improved comments, try to do it compatible for github.io

--- lib/cryptClass.py
+++ lib/cryptClass.py
@@ -2,41 +2,57 @@ from lib.header import *
 import base64
 import sys
 from Crypto.Hash import SHA512
-#'cryptoclass - cm = cryptoMeta'
+
 class cm:
+    """
+    Crypto-Meta-Class, should be able to do any symetric encryption and decryption for any text-data.
+    """
     def __init__(self, pyCryptoModule,  key=""):
-        self.key = key
-        self.mod = pyCryptoModule
-        if self.mod is not None:
-          self.name = self.mod.__name__[14:]
-        else:
-          self.name = "None"
-        if self.key != "encryptionInit" and self.key != "":
-            self.setKey(self.key)
-        logger.debug(tr("|Crypt| Init cm/cryptoMeta with module ")+self.name)
-        from Crypto import Random as rand
-        self.rand = rand
+      """
+      Keywords:
+      pyCryptoModule -- get a valid cryptoModule by scm.getMod
+      key -- is the passPhrase or also password
+      """
+      self.key = key
+      self.mod = pyCryptoModule
+      if self.mod is not None:
+        self.name = self.mod.__name__[14:]
+      else:
+        self.name = "None"
+      if self.key != "encryptionInit" and self.key != "":
+          self.setKey(self.key)
+      logger.debug(tr("|Crypt| Init cm/cryptoMeta with module ")+self.name)
+      from Crypto import Random as rand
+      self.rand = rand
 
     def setKey(self, key):
-        if len(key) is not 0:
-          hash = SHA512.new(bytes(key,'ascii'))
-          self.key = hash.hexdigest()
-          if self.name == "CAST" or self.name=="IDEA":
-            self.key = self.key[0:16]
-          elif self.name == "ARC2":
-            self.key = self.key[0:127]
-          elif self.name == "Blowfish"  or self.name == "ARC4":
-            self.key = self.key[0:56]
-          elif self.name == "AES" or self.name == "XOR":
-            self.key = self.key[0:32]
-          elif self.name == "DES3":
-            self.key = self.key[0:24]
-          return self.key
-        else:
-          self.mod = None
-          self.name = "None"
+      """
+      self.setKey does hash the password and shortens the hash to max of algorithm's key-block_size.
+      return new Key
+      """
+      if len(key) is not 0:
+        hash = SHA512.new(bytes(key,'ascii'))
+        self.key = hash.hexdigest()
+        if self.name == "CAST" or self.name=="IDEA":
+          self.key = self.key[0:16]
+        elif self.name == "ARC2":
+          self.key = self.key[0:127]
+        elif self.name == "Blowfish"  or self.name == "ARC4":
+          self.key = self.key[0:56]
+        elif self.name == "AES" or self.name == "XOR":
+          self.key = self.key[0:32]
+        elif self.name == "DES3":
+          self.key = self.key[0:24]
+        return self.key
+      else:
+        self.mod = None
+        self.name = "None"
 
     def encrypt(self, rawMessage):
+      """
+      Encrypt plaintext.
+      return encryptedMessage
+      """
       try:
         message=str(rawMessage)
         if self.mod == None and self.name=="None" and self.key != "encryptionInit":
@@ -61,6 +77,10 @@ class cm:
       except Exception as e:
         logger.error(tr("|Crypt| Encryptionerror: ")+ str(e.args[0])+tr(" Message ")+str(rawMessage))
     def decrypt(self, encryptedMessage):
+        """
+        Decrypt encrypted text.
+        return clearText
+        """
         try:
           if encryptedMessage is None:
               return ""
@@ -86,50 +106,61 @@ class cm:
         except Exception as e:
           logger.error(tr("|Crypt| Decryptionerror: ")+ str(e.args[0]))
 
-#static crypt manager
-class scm:
-    @staticmethod
-    def migrateEncryptionData(newCryptManager, toxMessagesHandler):
-      logger.debug("|Crypt|Start migrateEncryptionData")
-      toxMessagesHandler.saveAllMessages(newCryptManager)
-      toxMessagesHandler.eo=newCryptManager
-      toxMessagesHandler.updateMessages()
-          
 
-    @staticmethod
-    def getMod(configValue):
-        configValue = configValue.lower()
-        try:
-            if configValue == "1" or configValue == "cast":
-                if("CAST" not in sys.modules):
-                    from Crypto.Cipher import CAST
-                return CAST
-            elif configValue == "2"  or configValue == "blowfish":
-                if("Blowfish" not in sys.modules):
-                    from Crypto.Cipher import Blowfish
-                return Blowfish
-            elif configValue == "3"  or configValue == "des3":
-                if("DES3" not in sys.modules):
-                    from Crypto.Cipher import DES3
-                return DES3
-            elif configValue == "4"  or configValue == "arc4":
-                if("ARC4" not in sys.modules):
-                    from Crypto.Cipher import ARC4
-                return ARC4
-            elif configValue == "5"  or configValue == "xor":
-                if("XOR" not in sys.modules):
-                    from Crypto.Cipher import XOR
-                return XOR
-            elif configValue == "6"  or configValue == "aes":
-                if("AES" not in sys.modules):
-                    from Crypto.Cipher import AES
-                return AES
-            elif configValue == "7"  or configValue == "ardbCursor":
-                if("ARC2" not in sys.modules):
-                    from Crypto.Cipher import ARC2
-                return ARC2
-            else:
-                return None
-        except  Exception as e:
-            logger.error(tr("Could not find pycrypt-Module. Please install it via apt-get install python3-crypto"))
+class scm:
+  """
+  Static crypt manager, just contains static methods in context to encryption/decryption-stuff. 
+  """
+  @staticmethod
+  def migrateEncryptionData(newCryptManager, toxMessagesHandler):
+    """
+    In this case, there are just messages to migrate..
+    return None
+    """
+    logger.debug("|Crypt|Start migrateEncryptionData")
+    toxMessagesHandler.saveAllMessages(newCryptManager)
+    toxMessagesHandler.eo=newCryptManager
+    toxMessagesHandler.updateMessages()
+        
+
+  @staticmethod
+  def getMod(configValue):
+    """
+    get cryptomodule for cm. This is specialised for values, which you should enter in config.
+    return cryptoModule or None in case of not found.
+    """
+    configValue = configValue.lower()
+    try:
+        if configValue == "1" or configValue == "cast":
+            if("CAST" not in sys.modules):
+                from Crypto.Cipher import CAST
+            return CAST
+        elif configValue == "2"  or configValue == "blowfish":
+            if("Blowfish" not in sys.modules):
+                from Crypto.Cipher import Blowfish
+            return Blowfish
+        elif configValue == "3"  or configValue == "des3":
+            if("DES3" not in sys.modules):
+                from Crypto.Cipher import DES3
+            return DES3
+        elif configValue == "4"  or configValue == "arc4":
+            if("ARC4" not in sys.modules):
+                from Crypto.Cipher import ARC4
+            return ARC4
+        elif configValue == "5"  or configValue == "xor":
+            if("XOR" not in sys.modules):
+                from Crypto.Cipher import XOR
+            return XOR
+        elif configValue == "6"  or configValue == "aes":
+            if("AES" not in sys.modules):
+                from Crypto.Cipher import AES
+            return AES
+        elif configValue == "7"  or configValue == "arc2":
+            if("ARC2" not in sys.modules):
+                from Crypto.Cipher import ARC2
+            return ARC2
+        else:
             return None
+    except  Exception as e:
+        logger.error(tr("Could not find pycrypt-Module. Please install it via apt-get install python3-crypto"))
+        return None
--- lib/toxModels.py
+++ lib/toxModels.py
@@ -1,21 +1,36 @@
 from lib.header import *
 from PyQt4.QtCore import pyqtSlot,pyqtSignal
-# The next 3 classes are just to organize data. toxmessages is a message, 
+# The next 4 classes are just to organize data. toxmessages is a message, 
 # only special is that toxmessages by groupusers dont have dbid (theyr just in ram), but individual name
 class toxMessage:
+  """
+  A data-message-class handled by tox.
+  """
   def __init__(self,friendId,message, timestamp,me,dbId=-1,individualName=""):
-      if me == False or me == "False":
-        self.me = "False"
-      else:
-        self.me = "True"
-      self.friendId=friendId
-      self.message = message
-      self.timestamp = timestamp
-      self.dbId=dbId
-      self.individualName=individualName
+    """
+    dbId is only setted when it's reloaded min. once from db (only toxuser, but not groupuser has db)
+    individualName is only setted when user is a group. It's done to make other names possible than GroupBot #?
+    """
+    if me == False or me == "False":
+      self.me = "False"
+    else:
+      self.me = "True"
+    self.friendId=friendId
+    self.message = message
+    self.timestamp = timestamp
+    self.dbId=dbId
+    self.individualName=individualName
       
 class toxFile:
+  """
+  A data-file-class handled by tox.
+  """
   def __init__(self,id,filename,folder,size,fileObject,sr):
+    """
+    id -- fileId or file_number
+    fileObject -- any io-object created on accept request or send in controller
+    sr -- send recive. 0 = send, 1 = recive
+    """
     self.id = id
     self.filename = filename
     self.folder = folder
@@ -23,9 +38,11 @@ class toxFile:
     self.sr = sr
     self.splitSize=-1
     self.fileObject = fileObject
-    
-#toxUser is a individual, in groupchat or saved, single friend
+
 class toxUser:
+  """    
+  toxUser is a individual in binary/dual/non-group-chat. It's a data-object too.
+  """
   def __init__(self,friendId,name,pubKey,status,statusMessage):
     self.friendId = friendId
     self.name = name
@@ -37,35 +54,58 @@ class toxUser:
     self.files = []
     
   def getFileById(self,id):
+    """
+    Get a toxFile-Object by it's fileId
+    return toxFile or None
+    """
     tf = None
     for toxFile in self.files:
       if toxFile.id == id:
         return toxFile
 #a special version of toxuser, representing a group, which contains itself a list of toxUser
 class toxGroupUser(toxUser):
+  """
+  A groupUser inherits from toxUser, because they are to big parts similiar.
+  You could make a diffrent by static variable isGroup.
+  """
   def __init__(self,friendId,name,pubKey,status,statusMessage,peerList=[]):
     toxUser.__init__(self,friendId,name,pubKey,status,statusMessage)
     self.peerList = peerList
+    # This is a list of toxUser's
     self.messages = []
     self.checkedPeerIds = []
     self.isGroup = True
   def getPeerByName(self,name):
+    """
+    Get a temporary toxUser from groupchat.
+    return toxUser or None
+    """
     for peer in self.peerList:
       if peer.name == name:
         return peer
       else:
         return None
  
-#a handler for saving, manage and giving back the messages.
-# @TODO make this class enable/disable-able (?) in settings..
+
 class toxMessageHandler(QtCore.QObject):
+  """
+  a handler for saving, manage and giving back the messages.
+  @TODO make this class enable/disable-able (?) in settings.
+  """
   def __init__(self,eo):
+    """
+    eo -- encryptionObject - give it also when it's None!!
+    """
     QtCore.QObject.__init__(self)
     self.cachedToxMessages=[]
     self.tmpFriendId = -1
     self.eo = eo
-    
+  
   def saveAllMessages(self,eo):
+    """
+    reSave all messages. Mainly used by scm's method migrateEncryptedData
+    eo -- is the NEW encryptionObject
+    """
     for msg in self.updateMessages():
         if eo != None:
           if msg.me == "True":
@@ -78,17 +118,29 @@ class toxMessageHandler(QtCore.QObject):
     db.commit()
     
   def addMessage(self,toxMessage):
+    """
+    add toxMessage to the database
+    """
     if self.eo != None:
         dbCursor.execute("INSERT INTO messages (friendId, timestamp, message,me, encrypted) VALUES (?,?,?,?,?)",  ( self.eo.encrypt(toxMessage.friendId), self.eo.encrypt(toxMessage.timestamp),  self.eo.encrypt(toxMessage.message),self.eo.encrypt(toxMessage.me), self.eo.name))
     else:
         dbCursor.execute("INSERT INTO messages (friendId, timestamp, message,me, encrypted) VALUES (?,?,?,?,?)",  ( toxMessage.friendId, toxMessage.timestamp,  toxMessage.message,toxMessage.me, "-1"))
     db.commit()
+    
   def deleteUserMessages(self,friendId):
+    """
+    Delete a user's history/messages from database
+    """
     msgList = self.updateMessages(friendId)
     for msg in msgList:
       dbCursor.execute("DELETE FROM messages WHERE id=?",  (msg.dbId, ))
     db.commit()
   def updateMessages(self,friendId=-1):
+    """
+    updateMessages get messages from database to RAM.
+    friendId -- if setted -1 / you dont set it, it will load the messages of ALL users.
+    It's recommended to use a friendId!
+    """
     messages = []
     self.tmpDecryptData = []
     if friendId != -1:
